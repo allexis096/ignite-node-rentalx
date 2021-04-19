aa4cc0b6ca51577c047fd6a6e9de5ebcd30d01fc
allexis096 <allexissoares@gmail.com>
2021-04-18 22:07:31 -0300

    :sparkles: feat: add AppError middleware

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../../repositories/IUserRepository';
 
@@ -26,13 +27,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Email or password incorrect.');
+      throw new AppError('Email or password incorrect.');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Email or password incorrect.');
+      throw new AppError('Email or password incorrect.');
     }
 
     const token = sign({}, '8ad6c0fe5e869213a708351d1269393f', {
